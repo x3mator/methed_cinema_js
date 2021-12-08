661496c57f52d500241d0dffc07d5dbd48463681
x3mator <x3mator@gmail.com>
2021-12-09 01:46:45 +0200

    add burger menu

---
+++ js/index.js
@@ -0,0 +1,17 @@
+import slideMenu from './menu.js';
+
+
+slideMenu({
+  openBtn: '.header__burger-btn',
+  menu: '.navigation',
+  classActiveMenu: 'navigation_active',
+  closeTrigger: '.navigation__link, .navigation__close'
+})
+
+
+/*
+'.header__burger-btn',
+  '.navigation',
+  'navigation_active',
+  '.navigation__link, .navigation__close'
+ */
---
+++ js/menu.js
@@ -0,0 +1,25 @@
+const openMenu = (nav, active) => {
+  nav.classList.add(active);
+}
+const closeMenu = (nav, active) => {
+  nav.classList.remove(active)
+}
+
+const slideMenu = ({ openBtn, menu, classActiveMenu, closeTrigger}) => {
+
+  const burgerBtn = document.querySelector(openBtn)
+  const navigation = document.querySelector(menu)
+  const navigationClose = document.querySelectorAll(closeTrigger)
+
+  burgerBtn.addEventListener('click', () => {
+    openMenu(navigation, classActiveMenu)
+  })
+
+  navigationClose.forEach(item => {
+    item.addEventListener('click', () => {
+      closeMenu(navigation, classActiveMenu)
+    })
+  })
+}
+
+export default slideMenu
